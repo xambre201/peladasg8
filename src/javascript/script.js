52576a851525b48521a385981d12282c40241b04
xambre201 <grupog8kennedy@gmail.com>
2026-04-29 10:58:50 -0300

    Implement logout function in script.js
    Added logout function to handle session clearing and window closing.

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,10 @@
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
+
+function logout() {
+    // 1. Aqui você insere sua lógica de limpar sessão (ex: localStorage.clear())
+    alert("Efetuando logout...");
+
+    // 2. Tenta fechar a janela
+    window.close();
